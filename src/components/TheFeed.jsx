8441168ef170690c53124cb905bbcc2dfee2729f
Erik Holman <lifeinthedayof@gmail.com>
2024-11-06 13:50:24 -0800

    wireframe for feed

---
+++ src/components/TheFeed.jsx
@@ -0,0 +1,15 @@
+import './TheFeed.css';
+import EntryCard from './EntryCard';
+
+function TheFeed() {
+    <div className='the-feed'>
+        <EntryCard
+            author='tim'
+            updated='thursday'
+            link='http://www.google.com'
+            title='a title'
+        />
+    </div>;
+}
+
+export default TheFeed;
